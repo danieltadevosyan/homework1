88bb1d22c4567162e6947f7cba13a4d8788b958c
danieltadevosyan <danieltadevosyan95@gmail.com>
2023-08-12 16:38:41 +0400

    add first matrix task

---
+++ lesson 3/1.js
@@ -0,0 +1,46 @@
+// . Տրված է թվերի մատրիցա(զանվածների երկարթյնը կարող է տարբեր լինել) գտնել
+// ամենամեծ գմարը նեցող array-ի ինդեքսը:
+
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9],
+    [10, 11, 12, 13, 14]
+];
+
+const matrixArray = [[1, 5, 9, 8, 7], [8, 8, 9, 7, 8], [2, 6, 6, 6, 5]];
+
+
+
+
+function mergeArr(arr) {
+    const newArr = [];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            count += arr[i][j];
+        }
+        newArr.push(count);
+        count = 0;
+    }
+    return findMax(newArr);
+}
+
+
+function findMax(arr) {
+    let matrixValueIndex = 0;
+    let maxValue =  arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxValue) {
+            arr[i] = maxValue;
+            matrixValueIndex++;
+        } 
+    }
+    return matrixValueIndex
+       
+}
+
+console.log(mergeArr(matrix))
+
+console.log(mergeArr(matrixArray))
