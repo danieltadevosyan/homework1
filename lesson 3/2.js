6e4c76271d8c2288d7a66e5ad279907ab989d52d
danieltadevosyan <danieltadevosyan95@gmail.com>
2023-08-12 16:55:40 +0400

    add second matrix task

---
+++ lesson 3/2.js
@@ -0,0 +1,27 @@
+//. Տրված է թվերի մատրիցա (nxm), դրս բերել բոլոր տողերի մինիմմներից բաղկացած
+// զանգվածը:
+
+
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9],
+    [10, 11, 12, 13, 14]
+];
+
+const matrixArray = [[1, 5, 9, 8, 7], [8, 8, 9, 7, 8], [2, 6, 6, 6, 5]];
+
+function findMin(arr) {
+    const minima = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[i];
+        const minValue = Math.min(...row);
+        minima.push(minValue);
+    }
+
+    return minima;
+}
+
+console.log(findMin(matrix))
